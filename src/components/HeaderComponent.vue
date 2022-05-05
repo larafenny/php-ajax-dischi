bcf39d89b86b4a1e30d11e1f2396d7384e2a9fd7
Lara Filippone <lara.filippone@gmail.com>
2022-05-05 15:56:52 +0200

    created HeaderComponent and MainComponent

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        main
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainComponent'
+
+}
+</script>
+
+<style>
+
+</style>
